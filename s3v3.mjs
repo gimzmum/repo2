493812be63c982440f0ea60b34e5a8a9b5e47516
gimzmum <gimzmum@gmail.com>
2022-08-19 01:00:03 +0000

    added comment

--- s3v3.mjs
+++ s3v3.mjs
@@ -1,12 +1,13 @@
 
 // Import required AWS SDK clients and commands for Node.js.
+// Note use of .mjs extensions
 import { ListBucketsCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "./libs/s3Client.mjs"; // Helper function that creates an Amazon S3 service client module.
 
 export const run = async () => {
   try {
     const data = await s3Client.send(new ListBucketsCommand({}));
-    console.log("Success", data.Buckets);
+    console.log("Success!", data.Buckets);
     return data; // For unit tests.
   } catch (err) {
     console.log("Error", err);
